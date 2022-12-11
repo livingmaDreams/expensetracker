c5c5276595cfad228ba51bd1d5975ddb05cd9e61
Dream <sdeepicivil@gmail.com>
2022-12-11 18:34:28 +0530

    Expense tracker task 12

--- controllers/forgotpassword.js
+++ controllers/forgotpassword.js
@@ -2,6 +2,7 @@ const User = require('../models/users.js');
 const Forgotpassword = require('../models/forgotpassword');
 const sgMail = require('@sendgrid/mail');
 const uuid = require('uuid');
+const bcrypt = require('bcrypt');
 
 const path = require('path');
 
@@ -20,6 +21,13 @@ try{
   if(user)
   await user.createForgotpassword({id:id,active:'true',expiresby:current});
 
+  setTimeout(() =>{
+    Forgotpassword.findOne({where:{id:id}})
+    .then(user => {
+       return user.update({active:'false'})
+    }) 
+    .catch(err => console.log(err));
+  },3600000);
   
  const msg = {
   to: 'sdeepicivil@gmail.com', 
@@ -31,13 +39,43 @@ try{
 await sgMail.send(msg);
 
 res.status(200).json({Status:'true'});
+
 }
 catch(err){
+  console.log(err)
     res.status(500).json({Status:'false'});
 }
 }
 
 exports.getChangePwdPage = (req,res,next) =>{
+  const id = req.params.id;
+  
+  Forgotpassword.findOne({where:{id:id}})
+  .then(user =>{
+    if(user.active == 'true')
     res.sendFile(path.join(__dirname,`../views/changepassword.html`)); 
+    else
+    res.sendFile(path.join(__dirname,`../views/expiredlink.html`));
+  })
+   
+}
+
+exports.postResetPassword = async (req,res,next) =>{
+  const pwd = req.body.pwd;
+  const id = req.body.id;
+
+  try{
+    const user = await Forgotpassword.findOne({where:{id:id}});
+    const userid = user.userId;
+    const mainUser = await User.findOne({where:{id:userid}})
+    
+    bcrypt.hash(pwd,10,async (err,hash) =>{
+      await mainUser.update({password:hash});
+    })
+    res.status(201).json({passwordchanged: 'success'});
+  }
+  catch(err){
+    res.status(500).json({passwordchanged: 'failure'});
+  }
 }
 
